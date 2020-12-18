b76381443a833f6892fcad42f8f4c95ed001c90e
Chris Buckley <chris@cmbuckley.co.uk>
2020-12-18 21:59:16 +0000

    add last_modified changes in pre-commit hook

--- _ci/last-modified.sh
+++ _ci/last-modified.sh
@@ -10,4 +10,5 @@ $0 ~ "---" && ++count == 2 {
 
 while read -d $'\0' file; do
     awk -i inplace -v date="$(date +'%Y-%m-%d %H:%M %:z')" "$awkcmd" "$file"
+    git add "$file" # @todo avoid committing any other changes
 done
