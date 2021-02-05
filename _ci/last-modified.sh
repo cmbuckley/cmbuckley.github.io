54a2773cf7d4f9fb9d29e8f458447daec12771a6
Chris Buckley <chris@cmbuckley.co.uk>
2021-02-05 19:56:36 +0000

    update modified script to handle other Markdown pages

--- _ci/last-modified.sh
+++ _ci/last-modified.sh
@@ -8,7 +8,9 @@ $0 ~ "---" && ++count == 2 {
 !/^last_modified_at:/{print}
 '
 
+date="$(date +'%Y-%m-%d %H:%M %:z')"
+
 while read -d $'\0' file; do
-    awk -i inplace -v date="$(date +'%Y-%m-%d %H:%M %:z')" "$awkcmd" "$file"
+    awk -i inplace -v date="$date" "$awkcmd" "$file"
     git add "$file" # @todo avoid committing any other changes
 done
